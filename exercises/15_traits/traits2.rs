2848e8812790d220015420d0da6551aec63a2e4c
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2025-12-07 11:56:52 +0300

    feat: solve module 15

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -5,6 +5,13 @@ trait AppendBar {
 // TODO: Implement the trait `AppendBar` for a vector of strings.
 // `append_bar` should push the string "Bar" into the vector.
 
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".to_string());
+        self
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
@@ -15,7 +22,7 @@ mod tests {
 
     #[test]
     fn is_vec_pop_eq_bar() {
-        let mut foo = vec![String::from("Foo")].append_bar();
+        let mut foo: Vec<String> = vec![String::from("Foo")].append_bar();
         assert_eq!(foo.pop().unwrap(), "Bar");
         assert_eq!(foo.pop().unwrap(), "Foo");
     }
